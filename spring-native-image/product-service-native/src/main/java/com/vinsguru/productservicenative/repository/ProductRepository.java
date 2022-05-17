d0f631db59eff42b2f369d6c7d080d9f8c3e640b
Muhammad Mohsin <34002949+MMohsinM@users.noreply.github.com>
2022-05-17 14:22:42 +0500

    Delete spring-native-image/product-service-native directory

--- spring-native-image/product-service-native/src/main/java/com/vinsguru/productservicenative/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vinsguru.productservicenative.repository;
-
-import com.vinsguru.productservicenative.entity.Product;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends ReactiveMongoRepository<Product, Integer> {
-}
