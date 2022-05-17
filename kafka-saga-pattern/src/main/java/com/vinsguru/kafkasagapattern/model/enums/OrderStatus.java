0f63517082118bd3fb9fc9d178f52b5fbea6c347
Muhammad Mohsin <34002949+MMohsinM@users.noreply.github.com>
2022-05-17 14:22:14 +0500

    Delete kafka-saga-pattern directory

--- kafka-saga-pattern/src/main/java/com/vinsguru/kafkasagapattern/model/enums/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.vinsguru.kafkasagapattern.model.enums;
-
-public enum OrderStatus {
-
-    ORDER_CREATED,
-    ORDER_CANCELLED,
-    ORDER_COMPLETED
-
-}
--- kafka-saga-pattern/src/main/java/com/vinsguru/kafkasagapattern/model/enums/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.vinsguru.kafkasagapattern.model.enums;
-
-public enum PaymentStatus {
-
-    APPROVED,
-    REJECTED;
-
-}
