0d7bf030bd212bb865b951cd5af63651fc10638d
Muhammad Mohsin <34002949+MMohsinM@users.noreply.github.com>
2022-05-17 14:22:05 +0500

    Delete kafka-event-driven directory

--- kafka-event-driven/order-service/src/main/java/com/vinsguru/orderservice/service/UserServiceEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.vinsguru.orderservice.service;
-
-import com.vinsguru.orderservice.entity.User;
-
-public interface UserServiceEventHandler {
-    void updateUser(User user);
-}
--- kafka-event-driven/user-service/src/main/java/com/vinsguru/userservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.vinsguru.userservice.service;
-
-
-import com.vinsguru.userservice.dto.UserDto;
-
-public interface UserService {
-    Long createUser(UserDto userDto);
-    void updateUser(UserDto userDto);
-}
